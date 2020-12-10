07a14722fc50cd6ac1eb0a89e08efb04bae18bd1
Larisa Alexa <larisa.alexa@softvision.com>
2020-12-10 11:13:56 +0200

    Intelij VCS demo

--- src/test/java/clinic/programming/training/ApplicationTest.java
+++ src/test/java/clinic/programming/training/ApplicationTest.java
@@ -17,31 +17,31 @@ public class ApplicationTest {
     @Test
     public void testWordCountString() {
         int count = app.countWords("this is a test");
-        assertTrue(count == 4);
+        assertEquals(4, count);
     }
 
     @Test
     public void testWordCountEmpty() {
         int count = app.countWords("");
-        assertTrue(count == 0);
+        assertEquals(0, count);
     }
 
     @Test
     public void testWordCountNull() {
         int count = app.countWords(null);
-        assertTrue(count == 0);
+        assertEquals(0, count);
     }
 
     @Test
     @Ignore
     public void testWordCountMore() {
         int count = app.countWords("this will be skipped");
-        assertTrue(count == 3);
+        assertEquals(3, count);
     }
 
     @Test
     public void testWordCountSingle() {
         int count = app.countWords("test");
-        assertTrue(count == 1);
+        assertEquals(1, count);
     }
 }
